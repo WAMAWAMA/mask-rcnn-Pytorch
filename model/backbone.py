cd4f1978aa16d7edc81127c55bf3a68956824560
pkchen <pkchen>
2020-11-20 14:53:30 -0500

    backbone change

--- model/backbone.py
+++ model/backbone.py
@@ -57,7 +57,7 @@ def test():
 
     input = torch.Tensor(2, 3, 832, 928)
 
-    m = timm.create_model('cspresnet50', features_only=True, pretrained=True)
+    m = timm.create_model('AAcspresnet50', features_only=True, pretrained=True)
     m = TimmToVisionFPN(m)
 
     print(calculate_param(m))
